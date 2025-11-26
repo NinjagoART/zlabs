78e02221bcf5a6f9cb6271fed6d8e3473d8e8e07
NinjagoART <lloydmongomerigarmadon7@gmail.com>
2025-11-26 08:40:23 -0600

    ya

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import {Swiper} from 'swiper';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
